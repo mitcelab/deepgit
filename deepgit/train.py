fixed training to train on all repos, remove softmax

deepgit/train.py
@@ -39,23 +39,32 @@ c1 = encoder(repo_to_tensors[base_repo][1])
 num_samples = 5
 
 def run(epoch, mode = "train"):
-	base = encoder(repo_to_tensors[base_repo][0])
-	c1 = encoder(repo_to_tensors[base_repo][1])
-	similarity_scores = torch.zeros(1,num_samples+1)
-	similarity_scores[0][0] = F.cosine_similarity(base,c1)
+	loss = 0
+	score = 0
+	for r in repo_to_tensors.keys():
+		if len(repo_to_tensors[r]) > 1:
+			base_pair = random.sample(repo_to_tensors[r], 2)
 
-	comp_repos = list(repo_to_tensors.keys())
-	comp_repos.remove(base_repo)
+			base = encoder(base_pair[0])
+			c1 = encoder(base_pair[1])
+			similarity_scores = torch.zeros(1,num_samples+1)
+			similarity_scores[0][0] = F.cosine_similarity(base,c1)
 
-	for i in range(num_samples):
-		candidate = encoder(random.choice(repo_to_tensors[random.choice(comp_repos)]))
-		similarity_scores[0][i] = F.cosine_similarity(base,candidate)
+			comp_repos = list(repo_to_tensors.keys())
+			comp_repos.remove(r)
 
-	Y_pred = F.softmax(torch.tensor(similarity_scores), dim=1)
-	loss = F.cross_entropy(Y_pred, torch.LongTensor([0]))
-	score = int(torch.argmax(Y_pred).item() == 0)
+			for i in range(num_samples):
+				candidate = encoder(random.choice(repo_to_tensors[random.choice(comp_repos)]))
+				similarity_scores[0][i] = F.cosine_similarity(base,candidate)
 
-	print('iter', loss.item(), score)
+			Y_pred = torch.tensor(similarity_scores)
+			loss += F.cross_entropy(Y_pred, torch.LongTensor([0]))
+			score += int(torch.argmax(Y_pred).item() == 0)
+	loss /= len(repo_to_tensors)
+	score /= len(repo_to_tensors)
+
+	print('iter', epoch, loss.item(), score)
+	
 	if mode == "train":
 		loss.backward()
 		torch.nn.utils.clip_grad_norm_(encoder.parameters(), 0.25)
@@ -64,9 +73,10 @@ def run(epoch, mode = "train"):
 
 	if mode == "test":
 		torch.save(encoder, "weights/weights.epoch-%s.loss-%.03f.pt" % (epoch, loss.item()))
+		
 	return loss.item(), score
 
 for epoch in range(20):
-	# train_loss,train_acc = run(epoch, mode="train")
+	train_loss,train_acc = run(epoch, mode="train")
 	test_loss,test_acc = run(epoch, mode="test")
 	# print(train_loss,train_acc,test_loss,test_acc)