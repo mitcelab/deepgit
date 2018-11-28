a591b02172ca8243e09fa1fda692953a6eb40455
Aaron Huang <aaronhuang@Aarons-MacBook-Pro-2.local>
2018-11-28 02:16:09 -0500

    added watchers count

--- deepgit/data/get_urls.py
+++ deepgit/data/get_urls.py
@@ -6,11 +6,13 @@ PASSWORD=os.environ['gitpassword']
 
 data = []
 for p in range(10):
-	r = requests.get("https://api.github.com/search/repositories?q=crypto&size<10000&sort=stars&order=desc&page="+str(p)+"&per_page=100", auth=(USERNAME, PASSWORD))
-	print (len(r.json()['items']))
+	r = requests.get("https://api.github.com/search/repositories?q=crypto size:<50000&sort=stars&order=desc&page="+str(p)+"&per_page=100", auth=(USERNAME, PASSWORD))
+	# print (r.json()['items'][0].keys())
 	data.extend(r.json()['items'])
 	
-print (len(data))
+# print (data[0]['full_name'], data[0]['name'])
+names = open("github_names.txt","w")
 with open("github_urls.txt","w") as urls:
 	for d in data:
 		urls.write(d['clone_url']+'\n')
+		names.write(d['full_name'] + ' ' + str(d['watchers_count']) + ' ' + str(d['stargazers_count']) + '\n')
