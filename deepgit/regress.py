d3bdd879b8c40d51cabe769de5ded9e3bc4c6657
Aaron Huang <aaronhuang@Aarons-MacBook-Pro-2.local>
2018-12-01 16:39:43 -0500

    cleaned regression code

--- deepgit/regress.py
+++ deepgit/regress.py
@@ -3,65 +3,45 @@ from sklearn import linear_model
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+from enum import IntEnum
 import math
-
 import pickle
 
-y_f = open('labels.p','rb')
-x_f = open('vecs.p','rb')
 
-X = pickle.load(x_f)
-Y = pickle.load(y_f)
-X = [x.detach().numpy() for x in X]
-watchers = [int(math.log10(y[0])) for y in Y]
-stargazers = [int(math.log10(y[1])) for y in Y]
+with open('vecs.p','rb') as file:
+    X = pickle.load(file)
+    X = [x.detach().numpy() for x in X]
+with open('labels.p','rb') as file:
+    Y_all = np.array(pickle.load(file))
 
-watchersl = [y[0] for y in Y]
-stargazersl = [y[1] for y in Y]
 
-x,xv,y,yv = train_test_split(X, stargazers,test_size=0.1)
-xl,xlv,yl,ylv = train_test_split(X, stargazersl,test_size=0.1)
+class Metric(IntEnum):
+    WATCHERS=0
+    STARGAZERS=1
 
-reg_linear = linear_model.LinearRegression()
-reg_linear.fit(x,y)
 
-reg_log = linear_model.LogisticRegression(penalty='l2', C=10)
-reg_log.fit(x, y)
+def evaluate(model, target, use_log=False):
+    global Y_all
+    Y = Y_all[:,int(target)]
 
-rf = RandomForestRegressor(n_estimators = 100, random_state = 0)
-rf.fit(x, y)
+    if use_log:
+        Y = np.log(Y)
 
-plin = reg_linear.predict(xv)
-plog = reg_log.predict(xv)
-pred = rf.predict(xv)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+    model.fit(X_train, Y_train)
+    r2_train = r2_score(Y_train, model.predict(X_train))
+    r2_test = r2_score(Y_test, model.predict(X_test))
+    print(model, target) 
+    print (r2_train, r2_test)
 
-print('ERROR')
+# Watchers
+evaluate(linear_model.LinearRegression(), target=Metric.WATCHERS)
+evaluate(RandomForestRegressor(n_estimators=100), target=Metric.WATCHERS)
+evaluate(RandomForestRegressor(n_estimators=100), target=Metric.WATCHERS, use_log=True)
 
-error = []
-for i in range(len(ylv)):
-	error.append(abs(ylv[i]-plin[i])/ylv[i])
-print (np.mean(error))
-
-error = []
-for i in range(len(yv)):
-	error.append(abs(yv[i]-plog[i])/yv[i])
-print (np.mean(error))
-
-error = []
-for i in range(len(yv)):
-	error.append(abs(yv[i]-pred[i])/yv[i])
-print (np.mean(error))
-
-print('VAL')
-
-print (r2_score(pred, yv))
-print (r2_score(plog, yv))
-print (r2_score(plin, yv))
-
-print('TRAIN')
-
-print (r2_score(rf.predict(x), y))
-print (r2_score(reg.predict(x), y))
-print (r2_score(reg_linear.predict(x), y))
+# Stargazers
+evaluate(linear_model.LinearRegression(), target=Metric.STARGAZERS)
+evaluate(RandomForestRegressor(n_estimators=100), target=Metric.STARGAZERS)
+evaluate(RandomForestRegressor(n_estimators=100), target=Metric.WATCHERS, use_log=True)
 
 
