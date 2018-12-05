2ef9d3b1314e5d48332873f6e96ffae8c8ee12a6
Aaron Huang <aaronhuang@Aarons-MacBook-Pro-2.local>
2018-12-05 08:05:27 -0500

    Final list of urls

--- deepgit/data/get_urls.py
+++ deepgit/data/get_urls.py
@@ -6,15 +6,24 @@ USERNAME=os.environ['gitusername']
 PASSWORD=os.environ['gitpassword']
 
 data = []
-for p in range(1,4):
-	r = requests.get("https://api.github.com/search/repositories?q=topic:Cryptocurrency+language:python&language:go&language:cpp&language:c+stars:>100+size:<100000&sort=stars&order=desc&page="+str(p)+"&per_page=100", auth=(USERNAME, PASSWORD))
+for p in range(1,15):
+	r = requests.get("https://api.github.com/search/repositories?q=topic:Cryptocurrency+size:<100000&sort=stars&order=desc&page="+str(p)+"&per_page=100", auth=(USERNAME, PASSWORD))
+	if 'items' not in r.json():
+		break
 	data.extend(r.json()['items'])
 
 count_d = {}
 with open("github_urls.txt","w") as urls:
 	for d in data:
-		urls.write(d['clone_url']+'\n')
-		count_d[d['name']] = [d['watchers_count'], d['stargazers_count']]
+		if d['language'] and d['language'].lower() in {'python','go','c','c++','c#','javascript'}:
+			urls.write(d['clone_url']+'\n')
+			count_d[d['name']] = {
+				'stars':d['stargazers_count'],
+				'forks':d['forks_count'],
+				'score':d['score'],
+				'issues':d['open_issues']
+			}
+print (len(count_d))
 
-dfile = open("github_count_d.p","wb")
+dfile = open("repo_stats_d.p","wb")
 pickle.dump(count_d, dfile, protocol=pickle.HIGHEST_PROTOCOL)
