c02c1f536a48ee6d9daeed2f6ee0d4985dd25363
Aaron Huang <aaronhuang@Aarons-MacBook-Pro-2.local>
2018-12-06 03:46:58 -0500

    modify regress

--- deepgit/regress.py
+++ deepgit/regress.py
@@ -6,24 +6,24 @@ from sklearn.metrics import r2_score
 from enum import IntEnum
 import math
 import pickle
+import torch
 
 
-with open('vecs.p','rb') as file:
+with open('data/final/x_encoded.p','rb') as file:
     X = pickle.load(file)
     X = [x.detach().numpy() for x in X]
-with open('labels.p','rb') as file:
-    Y_all = np.array(pickle.load(file))
+with open('data/final/y_stats.p','rb') as file:
+    Y_stats = pickle.load(file)
 
 
 class Metric(IntEnum):
     WATCHERS=0
     STARGAZERS=1
 
-print (Y_all)
+print (Y_stats)
 
 def evaluate(model, target, use_log=False):
-    global Y_all
-    Y = Y_all[:,int(target)]
+    Y = [repo['stars'] for repo in Y_stats]
 
     if use_log:
         Y = np.log(Y)
@@ -32,7 +32,7 @@ def evaluate(model, target, use_log=False):
     model.fit(X_train, Y_train)
     r2_train = r2_score(Y_train, model.predict(X_train))
     r2_test = r2_score(Y_test, model.predict(X_test))
-    print(model, target) 
+    print(model, target)
     print (r2_train, r2_test)
 
 # Watchers
@@ -46,5 +46,3 @@ evaluate(linear_model.LinearRegression(), target=Metric.STARGAZERS)
 evaluate(linear_model.LinearRegression(), target=Metric.STARGAZERS, use_log=True)
 evaluate(RandomForestRegressor(n_estimators=100), target=Metric.STARGAZERS)
 evaluate(RandomForestRegressor(n_estimators=100), target=Metric.STARGAZERS, use_log=True)
-
-
